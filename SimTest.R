141ac9683ca307a8c9af41925e289914eca169c3
bw4sz <benweinstein2010@gmail.com>
2016-10-14 14:39:56 -0700

    working on gamma

--- SimTest.R
+++ SimTest.R
@@ -1,2 +1,2 @@
-a<-traj(gamma=c(1,0.4),theta=c(0,6),a1=inv.logit(c(1,-1)),total_time = 200,step_length=12)
+a<-traj(gamma=c(0.3,0.2),theta=c(0,6),a1=inv.logit(c(1,-1)),total_time = 200,step_length=12)
 ggplot(a,aes(x=x,y=y)) + theme_bw()  + geom_point(aes(col=State),size=1)  + theme(axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank()) + labs(x="",y="") + geom_path(aes(col=State,group=1),size=0.5) 
