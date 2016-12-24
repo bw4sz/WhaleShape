274990617f32d7b1b7777e8a5131cce1e0d02d94
bw4sz <benweinstein2010@gmail.com>
2016-12-24 08:10:15 -0800

    small edits

--- PlotOneTrack.R
+++ PlotOneTrack.R
@@ -1,5 +1,5 @@
-f<-mxy %>% filter(Animal==28,Track %in% c(1,2,3,4))
+f<-mxy %>% filter(Animal==9,Track %in% 12)
 
 d<-SpatialPointsDataFrame(cbind(f$x,f$y),data=f,proj4string=CRS("+proj=longlat +datum=WGS84"))
-ggplot(f,aes(x=x,y=y,col=phistate)) + geom_path(aes(group=Track),size=1) + borders(fill="black") + coord_cartesian(xlim=as.numeric(bbox(d)[1,]),ylim=as.numeric(bbox(d)[2,])) + theme_bw() + facet_wrap(~Track)
+ggplot(f,aes(x=x,y=y,col=phistate)) + borders(fill="black") + coord_cartesian(xlim=as.numeric(bbox(d)[1,]),ylim=as.numeric(bbox(d)[2,])) + theme_bw() + facet_wrap(~Track) + geom_path(aes(group=Track),size=1)
 
