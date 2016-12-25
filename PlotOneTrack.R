513f73aceb5b9e25ed644c4482c4d6d5479e391c
bw4sz <benweinstein2010@gmail.com>
2016-12-24 18:07:03 -0800

    checking amazon

--- PlotOneTrack.R
+++ PlotOneTrack.R
@@ -1,5 +1,4 @@
-f<-mxy %>% filter(Animal==9,Track %in% 12)
+f<-mxy %>% filter(Animal==11,Track %in% c(1,2,3))
 
 d<-SpatialPointsDataFrame(cbind(f$x,f$y),data=f,proj4string=CRS("+proj=longlat +datum=WGS84"))
-ggplot(f,aes(x=x,y=y,col=phistate)) + borders(fill="black") + coord_cartesian(xlim=as.numeric(bbox(d)[1,]),ylim=as.numeric(bbox(d)[2,])) + theme_bw() + facet_wrap(~Track) + geom_path(aes(group=Track),size=1)
-
+ggplot(f,aes(x=x,y=y,col=phistate)) + borders(fill="black") + coord_cartesian(xlim=as.numeric(bbox(d)[1,]),ylim=as.numeric(bbox(d)[2,])) + theme_bw() + facet_wrap(~Track,ncol=2) + geom_path(aes(group=Track),size=1)
