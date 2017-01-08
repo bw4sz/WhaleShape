190c3e485f84c588b8d2ea01f1cd5756bf9925b0
bw4sz <benweinstein2010@gmail.com>
2017-01-08 09:28:28 -0800

    experimentally testing the effect of 112748 on april results

--- PlotOneTrack.R
+++ PlotOneTrack.R
@@ -1,4 +1,4 @@
-a<-mxy %>% filter(Animal==2,Track %in% 2,Bout==25) %>% as.data.frame()
+a<-mxy %>% filter(Animal==28,Track %in% 1) %>% as.data.frame()
 
 d<-SpatialPointsDataFrame(cbind(a$x,a$y),data=a,proj4string=CRS("+proj=longlat +datum=WGS84"))
 ggplot(a,aes(x=x,y=y,col=phistate)) + borders(fill="black") + coord_cartesian(xlim=as.numeric(bbox(d)[1,]),ylim=as.numeric(bbox(d)[2,])) + theme_bw() + facet_wrap(~Track,ncol=2) + geom_path(aes(group=Track),size=1)
